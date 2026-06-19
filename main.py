9caf78a147cbb34ddc665ea09867222473677b71
deadguylegit <229603060+deadguylegit@users.noreply.github.com>
2026-06-19 19:54:02 +0530

    Added API calls and retrieved playlists.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from dotenv import dotenv_values
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import requests
+import json
 
-config=dotenv_values(r'D:\code\py\Youtify\client.env')
+config = dotenv_values(r"D:\code\py\Youtify\client.env")
 
-scope='playlist-read-private'
-sp=spotipy.Spotify(auth_manager=(SpotifyOAuth(client_id=config['SPOTIPY_CLIENT_ID'], client_secret=config['SPOTIPY_CLIENT_SECRET'], redirect_uri=config['SPOTIPY_REDIRECT_URL'], scope=scope)))
-results=sp.current_user_playlists()
-print(results)
+scope = "playlist-read-private"
+spotify = spotipy.Spotify(
+    auth_manager=(
+        SpotifyOAuth(
+            client_id=config["SPOTIPY_CLIENT_ID"],
+            client_secret=config["SPOTIPY_CLIENT_SECRET"],
+            redirect_uri=config["SPOTIPY_REDIRECT_URL"],
+            scope=scope,
+        )
+    )
+)
+user_id=spotify.current_user()['id']
+results = spotify.current_user_playlists()
+
+for result in results['items']:
+    if result['owner']['id']==user_id:
+        print(json.dumps(result, indent=1))
+
