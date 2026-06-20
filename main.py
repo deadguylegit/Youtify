769c82e39938b69665785002017166baec207827
deadguylegit <229603060+deadguylegit@users.noreply.github.com>
2026-06-20 23:19:42 +0530

    Refined structure, fixed bugs in track printing

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import json
 from dotenv import dotenv_values
 from spotipy.oauth2 import SpotifyOAuth
 
-def main():
-    spotify=authenticate_spotify()
-    playlists=get_playlists(spotify)
-    user_id=get_user_id(spotify)
-    
-    # tracks_list=get_tracks(spotify, '0UuAzJcX1PDq5BZtnnJgqg')
 
-    # i = 1
-    # for result in tracks_list:
-    #     for track in result['items']:
-    #         print(f'{i}. {track['item']['name']} by {track['item']['artists'][0]['name']}')
-    #         i += 1
-    print(spotify.playlist_items('0UuAzJcX1PDq5BZtnnJgqg', limit=10, offset=0))
+def main():
+    spotify = authenticate_spotify()
+    playlists = get_playlists(spotify)
+    user_id = get_user_id(spotify)
+    tracks = get_tracks(spotify, "0UuAzJcX1PDq5BZtnnJgqg")
+    # print(json.dumps(tracks, indent=2))
+
+    i = 1
+    for track in tracks:
+        print(f"{i}. {track['item']['name']} by {track['item']['artists'][0]['name']}")
+        i += 1
+    # print(json.dumps(spotify.playlist_items('0UuAzJcX1PDq5BZtnnJgqg', limit=10, offset=0), indent=2))
+
 
 def authenticate_spotify():
     config = dotenv_values(r"D:\code\py\Youtify\client.env")
@@ -32,25 +33,31 @@ def authenticate_spotify():
     )
     return auth_spotify
 
-def get_playlists(authenticated_spotify):   
+
+def get_playlists(authenticated_spotify):
     playlists = authenticated_spotify.current_user_playlists()
     return playlists
+
 
 def get_user_id(authenticated_spotify):
     user_id = authenticated_spotify.current_user()["id"]
     return user_id
 
-def get_tracks(authenticated_spotify, playlist_id):
-    # tracks=[]
-    # offset=0
-    # while True:
-    #     json_page=authenticated_spotify.playlist_items(playlist_id, limit=100, offset=offset)
-    #     for result in json_page:
 
-    #     if result['next']==None or result['next']=='null':
-    #         break
-    #     offset += 100 
-    # return tracks
+def get_tracks(authenticated_spotify, playlist_id):
+    tracks = []
+    offset = 0
+    while True:
+        json_page = authenticated_spotify.playlist_items(
+            playlist_id, limit=100, offset=offset
+        )
+        for result in json_page["items"]:
+            tracks.append(result)
+        if json_page["next"] is None:
+            break
+        offset += 100
+    return tracks
+
 
 if __name__ == "__main__":
     main()
